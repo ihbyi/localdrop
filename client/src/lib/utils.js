336ccdfe91b282021ad8c9ab9e5493f5cb9cb946
Husam Abdullah <dev.hso0om210@gmail.com>
2025-12-26 23:49:44 +0300

    remove decimals from size

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -11,5 +11,5 @@ export function formatSize(bytes) {
     const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
     const i = Math.floor(Math.log(bytes) / Math.log(k));
 
-    return `${(bytes / Math.pow(k, i)).toFixed(2)} ${sizes[i]}`;
+    return `${bytes / Math.pow(k, i)} ${sizes[i]}`;
 }
