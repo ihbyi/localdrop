23178cb3449a3dd6432e1c6f1926838bbdc4dd9c
Husam Abdullah <dev.hso0om210@gmail.com>
2025-12-26 08:13:00 +0300

    added sonner

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import { Button } from '@/components/ui/button';
+import { toast } from 'sonner';
 
 function App() {
     return (
         <div className="flex min-h-svh flex-col items-center justify-center">
-            <Button>Click me</Button>
+            <Button onClick={() => toast.success('Test')}>Click me</Button>
         </div>
     );
 }
